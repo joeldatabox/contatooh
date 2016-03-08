35ed58b2e222745c714a347215877f686ea0f47e
Joel Rodrigues Moreira <joel@maxxsoft.com.br>
2016-03-08 14:23:02 -0300

    configuração do Sauce Labs juntamente com o Travis

--- config/protractor.js
+++ config/protractor.js
@@ -1,13 +1,24 @@
 var config = require('./config')();
+
 exports.config = {
-    specs: ['../test/e2e/**/*Spec.js'],
-    onPrepare:function(){
-        browser.driver.get('http://localhost:3000')
-            .then(function(){
-                browser.driver.findElement(by.id('entrar')).click();
-                browser.driver.findElement(by.id('login_field')).sendKeys(config.seleniumUser);
-                browser.driver.findElement(by.id('password')).sendKeys(config.seleniumUserPassword);
-                browser.driver.findElement(by.name('commit')).click();
-            });
+    sauceUser : config.sauceUser,
+    sauceKey : config.sauceKey,
+    capabilities : {
+        'name': config.sauceTestName,
+        'browserName': 'chrome',
+        'tunnel-identifier': config.travisJobNumber,
+        'build': config.travisBuild
+    },
+    specs: ['../test/e2e/**/*.js'],
+    onPrepare: function() {
+        browser.driver.get('http://localhost:3000/#/auth').then(function() {
+            browser.driver.findElement(by.id('entrar')).click();
+            browser.driver.findElement(by.id('login_field'))
+                .sendKeys(config.seleniumUser);
+            browser.driver.findElement(by.id('password'))
+                .sendKeys(config.seleniumUserPassword);
+            browser.driver.findElement(by.name('commit')).click();
+        });
+
     }
 };
