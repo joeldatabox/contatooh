7c23021437574a15ec02891a299e751422e07c9d
Joel Rodrigues Moreira <joel@maxxsoft.com.br>
2016-03-08 09:02:33 -0300

    testando o travis

--- config/database.js
+++ config/database.js
@@ -1,15 +1,19 @@
 var mongoose = require('mongoose');
 module.exports = function (uri) {
     mongoose.connect(uri, {server: {poolSize: 1000}});
+
     mongoose.connection.on('connected', function () {
         console.log('Mongoose! Conectado em ' + uri);
     });
+
     mongoose.connection.on('disconnected', function () {
         console.log('Mongoose! Desconectado de ' + uri);
     });
+
     mongoose.connection.on('error', function () {
         console.log('Mongoose! Erro na conexão: ' + uri);
     });
+
     process.on('SIGINT', function () {
         mongoose.connection.close(function () {
             console.log('Mongoose! Desconectado pelo término da aplicação!');
